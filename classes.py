2bb162accdccc0c67642e3ba21ef1cdebeca75e9
Negevi <lukenicklemke@gmail.com>
2024-04-18 10:16:34 -0300

    Starting weapon impl

--- classes.py
+++ classes.py
@@ -3,27 +3,34 @@ import time
 from dataclasses import dataclass
 # Basic stats: AC, HP, Xp ( << class), Items (list of item class), weapon, money, and generals (name, race, sex?)
 class Stats:
-    def __init__(self, chosen_class): # 1 = melee 2 = mage 3 = ??
+    def __init__(self, chosen_class: int): # 1 = melee 2 = mage 3 = assasin
+        self.money = 20 # default stats
+        self.xp = 0
+        self.item = []
         if chosen_class == 1:
-            self.xp = 0
-            self.ac = 12
-            self.hp = die_roll(6, 8)
-            print(f"You have been granted {self.hp} Health Points!")
-            self.money = 20
-            self.weapon = None # empty vec or no info at all????
-            self.item = []
-
-class Generals:
-    def __init__(self):
-        self.name = input("For which arrangement of letters shall you be called adventurer? \n")
-        self.race = checker_option(3, "Pick a lineage of choice, dear traveler:\n [1] Human \n [2] Elf \n [3] Dwarf \n")
-        self.sex = checker_option(2, "And what are thou exacly?\n [1] Male \n [2] Female \n")
-
+            self.ac = 13
+            self.hp = 22
+            self.weapon = sword
+        elif chosen_class == 2:
+            self.ac = 11
+            self.hp = 16
+            self.weapon = staff
+        elif chosen_class == 3:
+            self.ac = 14
+            self.hp = 14
+            self.weapon = dagger
 class Player():
-    def __init__(self, chosen_class):
-        self.generals = Generals()
+    def __init__(self, chosen_class: int):
         self.stats = Stats(chosen_class)
-    def statcheck(player):
-        print(f"{player.generals.name}: \n Max hp: {player.stats.hp} \n Xp: {player.stats.xp} \n Armor class: {player.stats.ac} \n Gold: {player.stats.money} \n Equiped weapon(s): {player.stats.weapon} \n Item bag: {player.stats.item} \n")
+        
+    def __str__(player):
+        return f"Max hp: {player.stats.hp} \n Xp: {player.stats.xp} \n Armor class: {player.stats.ac} \n Gold: {player.stats.money} \n Equiped weapon(s): {player.stats.weapon} \n Item bag: {player.stats.item} \n"
 
-
+class Weapons():
+    def __init__(self, n: int, faces: int, dmg_mod: int, dual_wield: bool, spell_caster: bool, desc: str): # Note that dmg is defined as n + faces + flat modifier in a tuple.
+        self.dual_wield = dual_wield
+        self.spell_caster = spell_caster
+        self.special = None
+        self.desc = desc
+        self.dmg = (n, faces, dmg_mod)
+        
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# Start of a long journey, to S
-
 from engines import *
 from classes import *
 
-# Melee has extra Hp and. Mage has a Eldritch blast mechanic. 
+# Melee has extra Hp and. Mage has a Eldritch blast mechanic. Assasin can assasinate
 # Crit fail and Crit Max mechanic
 # Some sort of death save mehcanic?
 
-x = checker_option(2, "What would thou aspire to be in this adventure?\n [1] Melee \n [2] Mage \n [3] to be implemented\n ")
-player = Player(x)
-Player.statcheck(player)
+dmg = (4, 5)
+print(die_roll(dmg.0, dmg.1))
