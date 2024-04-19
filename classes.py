0b34171ca846b216d200d136349123d73d36426c
Negevi <lukenicklemke@gmail.com>
2024-04-19 10:38:03 -0300

    Finished basic weapons/new weapon impl

--- classes.py
+++ classes.py
@@ -1,6 +1,5 @@
 from engines import *
-import time
-from dataclasses import dataclass
+from generals import weapons
 # Basic stats: AC, HP, Xp ( << class), Items (list of item class), weapon, money, and generals (name, race, sex?)
 class Stats:
     def __init__(self, chosen_class: int): # 1 = melee 2 = mage 3 = assasin
@@ -10,15 +9,16 @@ class Stats:
         if chosen_class == 1:
             self.ac = 13
             self.hp = 22
-            self.weapon = sword
+            self.weapon = weapons["sword"]
         elif chosen_class == 2:
             self.ac = 11
             self.hp = 16
-            self.weapon = staff
+            self.weapon = weapons["staff"]
         elif chosen_class == 3:
             self.ac = 14
             self.hp = 14
-            self.weapon = dagger
+            self.weapon = weapons["dagger"]
+            
 class Player():
     def __init__(self, chosen_class: int):
         self.stats = Stats(chosen_class)
@@ -26,11 +26,23 @@ class Player():
     def __str__(player):
         return f"Max hp: {player.stats.hp} \n Xp: {player.stats.xp} \n Armor class: {player.stats.ac} \n Gold: {player.stats.money} \n Equiped weapon(s): {player.stats.weapon} \n Item bag: {player.stats.item} \n"
 
-class Weapons():
-    def __init__(self, n: int, faces: int, dmg_mod: int, dual_wield: bool, spell_caster: bool, desc: str): # Note that dmg is defined as n + faces + flat modifier in a tuple.
-        self.dual_wield = dual_wield
+class Weapons(): # intended for creation of new weapons
+    def __init__(self, dmg: tuple, spell_caster: bool, desc: str): # Note that dmg is defined as n + faces + flat modifier in a tuple.
+        self.desc = desc
+        self.dmg = (dmg[0], dmg[1], dmg[2])
         self.spell_caster = spell_caster
         self.special = None
-        self.desc = desc
-        self.dmg = (n, faces, dmg_mod)
         
+    def dictadd(self, name: str, dmg: tuple, spell_caster: bool, desc: str): # add special weapon suport
+        weapon = Weapons(dmg, spell_caster, desc)
+        weapons[name] = {
+            "desc": weapon.desc,
+            "dmg": weapon.dmg,
+            "spell_caster":  weapon.spell_caster,
+            "special": None
+        }
+    
+    def roll_dmg(self, weapon):
+        roll = die_roll(weapon.dmg[0], weapon.dmg[1]) + weapon.dmg[2]
+        print(f"You did {roll} dmg!")
+        return roll
---
+++ generals.py
@@ -0,0 +1,20 @@
+weapons: {
+    "sword": {
+        "desc": "A simple, rusty, sword. Used mainly for decoration!",
+        "dmg": (2, 8, 0),
+        "spell_caster":  False,
+        "special": None
+    }, 
+    "staff": {
+        "desc": "Wood. Thats it.",
+        "dmg": (1, 9, 0),
+        "spell_caster":  True,
+        "special": None
+    },
+    "dagger": {
+        "desc": "A shitty kitchen knife, scary even to the mightiest tomatoes. who even called this a dagger?",
+        "dmg": (4, 4, 0),
+        "spell_caster":  False,
+        "special": None
+    }, 
+} # type: ignore
