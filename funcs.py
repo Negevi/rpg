585760708b27d7d3e546963a2bc6565dfc854ed7
Negevi <lukenicklemke@gmail.com>
2024-05-22 11:04:38 -0300

    organization and error fixing

--- funcs.py
+++ funcs.py
@@ -1,18 +1,19 @@
-from dicts import (weapons, enemys)
+from dicts import weapons, enemys
 import random
-# Basic stats: AC, HP, Xp ( << class), Items (list of item class), weapon, money, and generals (name, race, sex?)
+
+# Basic stats: AC, HP, XP ( << class), Items (list of item class), weapon, money, and generals (name, race, sex?)
 class Stats:
-    def __init__(self, chosen_class: int): # 1 = melee 2 = mage 3 = assasin
-        self.money: int = 20 
+    def __init__(self, chosen_class: int):  # 1 = melee, 2 = mage, 3 = assassin
+        self.money: int = 20
         self.xp: int = 0
-        self.items: list = [] # default stats
+        self.items: list = []  # default stats
         if chosen_class == 1:
             self.ac: int = 13
             self.hp: int = 22
             self.weapon = weapons["sword"]
-            self.abilities = "Rage" # to add player abilities
+            self.abilities = "Rage"  # to add player abilities
         elif chosen_class == 2:
-            self.a: int = 11
+            self.ac: int = 11
             self.hp: int = 16
             self.weapon = weapons["staff"]
             self.abilities = "Cast Spell"
@@ -20,54 +21,56 @@ class Stats:
             self.ac: int = 14
             self.hp: int = 14
             self.weapon = weapons["dagger"]
-            self.abilities = "Assasinate"
-            
-class Player():
+            self.abilities = "Assassinate"
+
+class Player:
     def __init__(self, chosen_class: int):
         self.stats = Stats(chosen_class)
-        
-    def __str__(player):
-        return f"Max hp: {player.stats.hp}\n" \
-           f"XP: {player.stats.xp}\n" \
-           f"Armor class: {player.stats.ac}\n" \
-           f"Gold: {player.stats.money}\n" \
-           f"Equipped weapon: {player.stats.weapon["name"]}\n   {player.stats.weapon["desc"]}\n" \
-           f"Item bag: {', '.join(player.stats.items)}"
-    
+
+    def __str__(self):
+        return (f"Max hp: {self.stats.hp}\n"
+                f"XP: {self.stats.xp}\n"
+                f"Armor class: {self.stats.ac}\n"
+                f"Gold: {self.stats.money}\n"
+                f"Equipped weapon: {self.stats.weapon['name']}\n   {self.stats.weapon['desc']}\n"
+                f"Item bag: {', '.join(self.stats.items)}\n")
+
     def turn(self, enemy):
-        check = checker_option(2, "What would you like to do?\n [0] use ability\n [1] Atack")
+        check = Engines.checker_option(2, "What would you like to do?\n [0] use ability\n [1] Attack")
         if check == 0:
-            print("use ability!")
+            print("Use ability!")
         if check == 1:
-            if die_roll(1, 20, self.stats.weapon.dmg[0]) >= enemy.ac:
+            if Engines.die_roll(1, 20) + self.stats.weapon["dmg"][0] >= enemy.ac:
                 print("Hit!")
                 dmg = Weapons.roll_dmg(self.stats.weapon)
                 enemy.hp -= dmg
             else:
                 print("Miss!")
-           
-class Weapons(): # intended for creation of new weapons
-    def __init__(self, dmg: tuple, spell_caster: bool, desc: str): # Note that dmg is defined as n + faces + flat modifier in a tuple.
+
+class Weapons:  # intended for creation of new weapons
+    def __init__(self, dmg: tuple, spell_caster: bool, desc: str):  # dmg is defined as (n, faces, flat modifier)
         self.desc = desc
         self.dmg = (dmg[0], dmg[1], dmg[2])
         self.spell_caster = spell_caster
         self.special = None
-        
-    def dictadd_weapon(self, name: str, dmg: tuple, spell_caster: bool, desc: str): # add special weapon suport
+
+    @staticmethod
+    def dictadd_weapon(name: str, dmg: tuple, spell_caster: bool, desc: str):  # add special weapon support
         weapon = Weapons(dmg, spell_caster, desc)
         weapons[name] = {
             "desc": weapon.desc,
             "dmg": weapon.dmg,
-            "spell_caster":  weapon.spell_caster,
+            "spell_caster": weapon.spell_caster,
             "special": None
         }
-    
-    def roll_dmg(self, weapon):
-        roll = die_roll(weapon.dmg[0], weapon.dmg[1]) + weapon.dmg[2]
-        print(f"You did {roll} dmg!")
-        return roll      
 
-class Enemy():
+    @staticmethod
+    def roll_dmg(weapon):
+        roll = Engines.die_roll(weapon["dmg"][0], weapon["dmg"][1]) + weapon["dmg"][2]
+        print(f"{roll} dmg!")
+        return roll
+
+class Enemy:
     def __init__(self):
         enemy = Enemy.get_enemy()
         self.name = enemy["name"]
@@ -76,95 +79,107 @@ class Enemy():
         self.weapon = enemy["weapon"]
         self.abilities = enemy["abilities"]
         self.ac = enemy["ac"]
-        
+
     def __str__(self):
-        return f"{self.name}\n" \
-        f"Hp: {self.hp}\n" \
-        f"Description: {self.desc}\n" \
-        f"Weapon: {self.weapon['name']}\n" \
-        f"Ability(s): {self.abilities['name']}\n {self.abilities['desc']}\n"
-        
-    @staticmethod          
+        return (f"{self.name}\n"
+                f"Hp: {self.hp}\n"
+                f"Description: {self.desc}\n"
+                f"Weapon: {self.weapon['name']}\n"
+                f"Ability(s): {self.abilities['name']}\n {self.abilities['desc']}\n")
+
+    def turn(self, player):
+        if Engines.die_roll(1, 6) == 6:  # here, if 1d6 = 6, use ability
+            print("Ability!")
+        else:
+            dmg = Weapons.roll_dmg(self.weapon)
+            player.stats.hp -= dmg
+
+    @staticmethod
     def get_enemy():
+        key_randomizer = random.randint(0, len(enemys) - 1)
         key_id = 0
-        key_randomizer = random.randint(0, 2) # max of monsters
         for key in enemys.keys():
             if key_randomizer == key_id:
                 return enemys.get(key)
             else:
                 key_id += 1
-    
+
     @staticmethod
     def level(enemy, lvl):
-        multiplier =  int(1 + lvl / 4) # to change, dont know yet
-        enemy['hp'] * multiplier
+        multiplier = int(1 + lvl / 4)  # to change, don't know yet
+        enemy['hp'] *= multiplier
         return enemy
-    
-    @staticmethod    
+
+    @staticmethod
     def gen_fight(Plvl):
-        to_lvl(Plvl)
+        Engines.to_lvl(Plvl)
         hostiles = []
-        i = Plvl
-        for i in range(0, Plvl):
+        for _ in range(Plvl):
             hostiles.append(Enemy())
-            i -= 1
         return hostiles
-    
+
     @staticmethod
     def print_hostiles(hostiles):
-        id = 0
-        for enemy in hostiles:
+        for id, enemy in enumerate(hostiles):
             print(f"{enemy}\n id={id}")
-            id += 1
-
-def checker_option(n, statement):
-    x = int(input(statement))
-    if x < 0 or x >= n:
-        checker_option(n, statement)
-    else:
-        return x
-
-# nDfaces
-def die_roll(n, faces):
-    die_roll = 0
-    p = n
-    while n > 0:
-        roll = random.randint(1, faces)
-        die_roll += roll
-        print(f"{p}D{roll},")
-        n -= 1
-    print(f"total: {die_roll}")
-    return die_roll
-
-def adv_roll(x, y):
-    if x >= y:
-        return x
-    else:
-        return y
-
-def dsv_roll(x, y):
-    if x <= y:
-        return x
-    else:
-        return y
-    
-def to_lvl(xp):
-    lvl = 0
-    while xp > 1:
-        lvl += round(xp / 2, 0) # lvl up system, xp amount raises by 2x 
-    return lvl + 1
 
 def encounter(player):
-    print("A fight will begin!")
-    hostiles: list = Enemy.gen_fight(player.stats.xp)
-    Enemy.print_hostiles(hostiles)
-    init = die_roll(1, 20)
-    print("init!")
-    while (player.stats.hp or hostiles.len()) != 0:
+    hostiles = Enemy.gen_fight(Engines.to_lvl(player.stats.xp))
+    init = Engines.die_roll(1, 20)
+    print("init!^")
+    while player.stats.hp > 0 and len(hostiles) > 0:
         if init >= 10:
-            Player.turn(player, hostiles[0])
-            for enemy in hostiles:
-                print("enemys turn!")
+            Enemy.print_hostiles(hostiles)
+            enemy = hostiles[Engines.checker_option(len(hostiles), "Which enemy will you attack? (Id)\n")]
+            player.turn(enemy)
+            if enemy.hp <= 0:
                 hostiles.remove(enemy)
 
+            for enemy in hostiles:
+                print(f"{enemy.name} turn!")
+                enemy.turn(player)
+        else:
+            for enemy in hostiles:
+                print(f"{enemy.name} turn!")
+                enemy.turn(player)
 
+            Enemy.print_hostiles(hostiles)
+            enemy = hostiles[Engines.checker_option(len(hostiles), "Which enemy will you attack? (Id)\n")]
+            player.turn(enemy)
+            if enemy.hp <= 0:
+                hostiles.remove(enemy)
+
+class Engines:
+    @staticmethod
+    def checker_option(n, statement):
+        x = int(input(statement))
+        if x < 0 or x >= n:
+            return Engines.checker_option(n, statement)
+        else:
+            return x
+
+    @staticmethod
+    def die_roll(n, faces):
+        die_roll = 0
+        for _ in range(n):
+            roll = random.randint(1, faces)
+            die_roll += roll
+            print(f"{n}D{roll},")
+        print(f"total: {die_roll}")
+        return die_roll
+
+    @staticmethod
+    def adv_roll(x, y):
+        return max(x, y)
+
+    @staticmethod
+    def dsv_roll(x, y):
+        return min(x, y)
+
+    @staticmethod
+    def to_lvl(xp):
+        lvl = 0
+        while xp > 1:
+            lvl += round(xp / 2, 1)  # lvl up system, xp amount raises by 2x
+            xp /= 2
+        return lvl + 1
--- main.py
+++ main.py
@@ -5,5 +5,4 @@ from funcs import *
 # Some sort of death save mehcanic?
 
 player = Player(checker_option(2, "Choose class: \n [1] Melee \n [2] Mage \n [3] Rogue\n"))
-print(player)
-encounter(player)
+Enemy.print_hostiles(Enemy.gen_fight(player.stats.xp))
